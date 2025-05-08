ecf2251ee188963e88cf7fd03c2042ba0d3fbcb9
Rishabh Agarwal <agarwalrishabh2005@gmail.com>
2025-05-07 18:15:08 -0600

    an algorithm for Reverse mode was added
    Signed-off-by: Rishabh Agarwal <agarwalrishabh2005@gmail.com>

--- src/autogradient.cpp
+++ src/autogradient.cpp
@@ -14,7 +14,7 @@ using namespace std;
 namespace simplenet{
     // TODO: This is just a placeholder for now
 
-    // NOtes for implementation 
+    // Notes for implementation 
     /*
 
     We will be using a topological sort to walk the graph and compute the derivatives
@@ -26,33 +26,27 @@ namespace simplenet{
     */
 
 
-    // binary operations 
-    struct Node {
-        double weights[2];
-        size_t deps[2]; 
-    };
+    // We need to create a representation for a computation graph
+    // then we can use a reverse topological order to walk the graph and compute the derivatives backward from the output
+    //    x1 ---- x3 ---- out 
+    //    x2-----/
+    //      here we have x1, x2, x3, out - we start the backward pass from out to compute the derivatives of out with respect to all the nodes 
+    // Algorithm is as follows:
 
-    class Variable {
-        size_t index;
-        double value;
-        std::vector<Node*> nodes; // store the pointers to the nodes to reduce the memory usage
-    public:
-        Variable(size_t index, double value) : index(index), value(value) {}
-
-        void addNode(Node* node) {
-            nodes.push_back(node);
-        }
-
-        friend Variable operator+(Variable a, Variable b) {
-            Variable result(a.index, a.value + b.value);
-            // TODO: 
-        }
-
-        operator const double() const {
-            return value;
-        }
-    };
+    /*
     
+    def gradient(out):
+        node_to_grad = {out: [1]} // dictionary for node to list of gradients for each node that is connected to it
+        
+        for i in reverse_topological_sort(out):
+            v_i_adjoint = sum(node_to_grad[i])
+
+            for k in inputs(i):
+                compute v_k_to_i_adjoint = v_i_adjoint * d_v_i / d_v_k
+                append v_k_to_i_adjoint to node_to_grad[k] // propagate the partial adjoints to its inputs 
+        return adjoint of input v_input(s) - this is the gradient of the function (network) with respect to all the inputs  
+    */
+
 
 }
 namespace simplenet::autogradient{
