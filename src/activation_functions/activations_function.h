90cf40341bb0b93cf1dc04b0b9554234f423e6fc
Rishabh Agarwal <agarwalrishabh2005@gmail.com>
2025-09-13 22:59:11 -0600

    kk
    Signed-off-by: Rishabh Agarwal <agarwalrishabh2005@gmail.com>

--- src/activation_functions/activations_function.h
+++ src/activation_functions/activations_function.h
@@ -6,7 +6,8 @@
 namespace simplenet {
     class Tensor; // forward declaration
     namespace activations_function {
-
+        // will apply polymorphism and inheritance for activation functions as the principle idea is the same
+        // will inherit the Node class for operations to be carried out and use the backward features
     }
 }
 #endif
