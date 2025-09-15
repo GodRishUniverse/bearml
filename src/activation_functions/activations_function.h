15637956be4da8a2e7993d398847d63b1059f0a7
Rishabh Agarwal <agarwalrishabh2005@gmail.com>
2025-09-14 18:30:03 -0600

    some more boilerplate for nn and activations and some restructuing
    Signed-off-by: Rishabh Agarwal <agarwalrishabh2005@gmail.com>

--- src/activation_functions/activations_function.h
+++ src/activation_functions/activations_function.h
@@ -11,7 +11,6 @@
 #ifndef ACTIVATION_FUNCTIONS
 namespace simplenet {
 
-
     namespace neural_network {
         // will apply polymorphism and inheritance for activation functions as the principle idea is the same
         // will inherit the Node class for operations to be carried out and use the backward features
