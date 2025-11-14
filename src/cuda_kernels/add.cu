bf697bad0e3afcbe07fa4eb117d7214ff7ac95b1
Rishabh Agarwal <agarwalrishabh2005@gmail.com>
2025-11-14 11:35:01 -0700

    removed false old kernel
    Signed-off-by: Rishabh Agarwal <agarwalrishabh2005@gmail.com>

--- src/cuda_kernels/add.cu
+++ src/cuda_kernels/add.cu
@@ -4,7 +4,5 @@ using ll = long long;
 
 __global__ void addTensors(Tensor* A, Tensor* B, Tensor* C) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < N) {
-        C[idx] = A[idx] + B[idx];
-    }
+    // need to think about how broadcasting addition may be done on the gpu
 }
