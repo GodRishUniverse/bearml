a900bb15304b8a945eb66774ff957c28db248995
Rishabh Agarwal <agarwalrishabh2005@gmail.com>
2026-01-28 22:14:19 -0700

    made a note about .cuh vs .cu in header files called by .cpp/.h files
    Signed-off-by: Rishabh Agarwal <agarwalrishabh2005@gmail.com>

--- simplenet/cuda/includes/kernel_links.h
+++ simplenet/cuda/includes/kernel_links.h
@@ -1,3 +1,6 @@
+// Never add ".cu" files here as GCC doesnt recognize them
+// It recognizes ".cuh" as CUDA header files so the NVCC can compile the ".cu" files
+
 #include "../kernels/element_wise_kernels.cuh"
 #include "../kernels/fill.cuh"
 #include "../kernels/matmul.cuh"
