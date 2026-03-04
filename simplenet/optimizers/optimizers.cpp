8c916e6c99e888c13a1d2eca132c9f95061c7889
Rishabh Agarwal <agarwalrishabh2005@gmail.com>
2026-03-03 23:03:31 -0700

    adam implementation started
    Signed-off-by: Rishabh Agarwal <agarwalrishabh2005@gmail.com>

--- simplenet/optimizers/optimizers.cpp
+++ simplenet/optimizers/optimizers.cpp
@@ -21,13 +21,23 @@ namespace simplenet {
                }
            }
 
-           // TODO: Complete these optimizers
-           Adam::Adam(std::vector<std::shared_ptr<simplenet::Node<simplenet::Tensor>>> params, double learning_rate, double momentum) : params(params), learning_rate(learning_rate), momentum(momentum){
+           // TODO: fix Adam implementation
+           // src - https://builtin.com/machine-learning/adam-optimization#:~:text=Momentum%20speeds%20up%20training%20by,need%20to%20take%20fewer%20steps.
+           Adam::Adam(std::vector<std::shared_ptr<simplenet::Node<simplenet::Tensor>>> params, double learning_rate, double momentum, double rms_prop, double beta1,  double beta2, double eps) : params(params), learning_rate(learning_rate), momentum(momentum), rms_prop(rms_prop), beta1(beta1), beta2(beta2), eps(eps), step_count(1){
 
            }
 
            void Adam::step(){
-               // TODO
+               for (auto p : this->params){
+                   momentum = beta1*momentum+(1-beta1)*p->grad;
+                   rms_prop = beta2*rms_prop+(1-beta2)*p->grad*p->grad;
+
+                   // bias correction
+                   double m_hat = momentum/(1-pow(beta1, this->step_count));
+                   double v_hat = rms_prop/(1-pow(beta2, this->step_count));
+                   p->val -= learning_rate*m_hat/(std::sqrt(v_hat)+eps);
+               }
+               this->step_count++;
            }
 
            void Adam::zero_grad(){
--- simplenet/optimizers/optimizers.h
+++ simplenet/optimizers/optimizers.h
@@ -37,9 +37,15 @@ namespace simplenet {
                     std::vector<std::shared_ptr<simplenet::Node<simplenet::Tensor>>> params;
                     double momentum;
                     double learning_rate;
+                    double rms_prop;
+                    double beta1; // decay rate for momentum
+                    double beta2; // decay rate for RMSProp
+                    double eps; // small value to avoid division by zero
+
+                    int64_t step_count;
 
                 public:
-                    Adam(std::vector<std::shared_ptr<simplenet::Node<simplenet::Tensor>>> params, double learning_rate= 0.0001, double momentum = 0.001);
+                    Adam(std::vector<std::shared_ptr<simplenet::Node<simplenet::Tensor>>> params, double learning_rate= 0.0001, double momentum = 0.001, double rms_prop = 0.999, double beta1 = 0.9, double beta2 = 0.999, double eps = 1e-8);
                     void step() override;
                     void zero_grad() override;
             };
