a2097cb8e44086c90dc46c5882b477da3272c1ce
Rishabh Agarwal <agarwalrishabh2005@gmail.com>
2026-03-03 23:06:59 -0700

    cuda block size fixed i think - need to check
    Signed-off-by: Rishabh Agarwal <agarwalrishabh2005@gmail.com>

--- simplenet/cuda/includes/cuda_consts.h
+++ simplenet/cuda/includes/cuda_consts.h
@@ -4,5 +4,5 @@
 
 // This is being added to make sure we can take advantage of memory coelscing (close memory accesses)
 #ifndef BLOCK_SIZE
-    #define BLOCK_SIZE 1024 // multiple of 32 for hardware efficiency reasons
+    #define BLOCK_SIZE 32 // multiple of 32 for hardware efficiency reasons - CUDA hard limit per block is 1024 threads i think so 32 by 32 is max - TODO: check this otherwise make difference consts forr linear, 2D and 3D kernel blocks
 #endif
