597e91a02d14e81e5e94874284ad577503064562
Rishabh Agarwal <agarwalrishabh2005@gmail.com>
2026-03-21 18:27:11 -0600

    ok so found the error - there are some rpm packages missing I think as std::abs gives me seg fault in the loss function
    Signed-off-by: Rishabh Agarwal <agarwalrishabh2005@gmail.com>

---
+++ debug.sh
@@ -0,0 +1,3 @@
+gdb ./build/runchecks/test_runner; # start gdb on the test runner executable
+# use bt in gdb to print the backtrace
+# r is used to run the program in gdb
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 cd build;
-cmake ..;
+cmake -DCMAKE_BUILD_TYPE=Debug ..; # we build in debug mode so that we can use gdb for debugging
 make;
 ./runchecks/test_runner;
