1e9e57a143896eafe44d3bece1996b1c862faf60
Rishabh Agarwal <agarwalrishabh2005@gmail.com>
2026-06-17 18:47:49 -0400

    thinking about how I can setup datasets and dataloading
    Signed-off-by: Rishabh Agarwal <agarwalrishabh2005@gmail.com>

---
+++ simplenet/dataloader/dataloader.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+
+// we wrill write our data loader here
+#ifndef SIMPLENET_DATALOADER_H
+#define SIMPLENET_DATALOADER_H
+
+
+namespace simplenet {
+    namespace data {
+        class Dataloader {
+        public:
+            Dataloader() {
+                // TODO:
+            }
+        };
+
+    }
+}
+
+#endif
---
+++ simplenet/datasets/datasets.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+
+// we wrill write our dataset class here
+#ifndef SIMPLENET_DATASETS_H
+#define SIMPLENET_DATASETS_H
+
+namespace simplenet {
+    namespace data {
+        class Dataset {
+        public:
+            Dataset() {
+                // TODO:
+            }
+        };
+
+    }
+}
+
+#endif
