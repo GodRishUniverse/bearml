c428f6e6ff8d113c8a84e6b5cf6d9254d1738c89
Rishabh Agarwal <agarwalrishabh2005@gmail.com>
2026-06-20 14:15:32 -0400

    made some progress in softmax kernel - learned tree style reduction is good to avoid race conditions on the GPU as atomic operations will serialize a lot of stuff
    Signed-off-by: Rishabh Agarwal <agarwalrishabh2005@gmail.com>

--- simplenet/cuda/kernels/softmax.cu
+++ simplenet/cuda/kernels/softmax.cu
@@ -1,5 +1,6 @@
 #include "softmax.cuh"
-#include "sum.cuh"
+#include <__clang_cuda_builtin_vars.h>
+#include <__clang_cuda_math.h>
 #include <cmath>
 #include <cstddef>
 #include <cstdint>
@@ -7,34 +8,60 @@
 namespace simplenet {
     namespace cuda {
 
-        // todo: think about how dim is used in softmax (I know per row is naive and default but generalization is good)
-        // todo: another problem is when the shared memory overlaps as not all data items on a CU/SM
-        // figure out how to handle this overlap
+        // softmax max trick is that you subtract the max value from each element before taking the exp (as it gives the same result as not doing that and makes sure large values don't overflow)
         template<typename T>
         __global__ void softmax_kernel(const T* d_data, T* d_out,  int* shape, int shape_size, int* stride, int stride_size, int dim, int64_t size) {
             int shape_dim = shape[dim];
             int stride_dim = stride[dim];
 
+            int row_base  = 0 ;
+            int group = blockIdx.x ;// This row is assigned to this block
+            // TODO: rowbase calculation
+
+
+            //
+
             // shared memory for cumulative sum  = number of rows basically
             // example is like 4*5 will need 1 float per row (4 floats)
             // so shape[dim] * sizeof(T) is the size of the shared memory per block
-            extern __shared__ T cum_sum_storage[]; // needs to be declared outside the kernel in the <<<...>>> call with the shared memory size (has to be shape[dim] * sizeof(T))
+            extern __shared__ T smemory[]; // needs to be declared outside the kernel in the <<<...>>> call with the shared memory size (has to be shape[dim] * sizeof(T))
 
-            for (size_t idx = blockIdx.x * blockDim.x + threadIdx.x;  idx < size; idx += blockDim.x * gridDim.x) {
-                // one thread per element
-                T temp = exp(d_data[idx]);
-                d_out[idx] = temp;
-                // idx is the global index, we use it to index into the shared memory
-                int local_idx = ceil(idx / shape_dim);
-                cum_sum_storage[local_idx] += temp;
-
+            // local max for each thread block (partial max)
+            T local_max = -std::numeric_limits<T>::infinity(); // in register
+            for (int i = threadIdx.x; i < shape_dim; i += blockDim.x) {
+                local_max = max(local_max, d_data[row_base+ i * stride_dim]);
             }
-            __syncthreads(); // I was thinking of using a shared memory reduction to do a cumulative sum and then divide by the sum
+            smemory[threadIdx.x] = local_max; // store local max in shared memory (avoids data race to use their own block of memory)
+            __syncthreads(); // synchronize to ensure all threads have written their local max to shared memory
+            // Carry out a tree reduction to find the global max (LLMs helped me figure this out as atomicMax would have serialized this to O(N) rather than O(log N))
+            for (int s = blockDim.x/2; s > 0; s >>= 1) {
+                if (threadIdx.x < s) smemory[threadIdx.x] = max(smemory[threadIdx.x], smemory[threadIdx.x+s]);
+                __syncthreads();
+            }
+            T row_max = smemory[0];
+            __syncthreads();
+
+            // similar to the tree reduction above, but for the local row sum
+
+            T local_row_sum = 0; // local sum for each thread block (partial sum)
+            for (int i = threadIdx.x; i < shape_dim; i += blockDim.x) {
+                local_row_sum += exp(d_data[row_base+   i * stride_dim]-row_max);;
+            }
+            smemory[threadIdx.x] = local_row_sum; // store local sum in shared memory (avoids data race to use their own block of memory)
+            __syncthreads(); // synchronize to ensure all threads have written their local sum to shared memory
+
+            for (int s = blockDim.x/2; s > 0; s >>= 1) {
+                if (threadIdx.x < s)
+                    smemory[threadIdx.x] = smemory[threadIdx.x] + smemory[threadIdx.x+s];
+                __syncthreads();
+            }
+            T row_sum = smemory[0];
+            __syncthreads();
 
             // apply the divide after the cumulative sum is computed
-            for (size_t idx = blockIdx.x * blockDim.x + threadIdx.x;  idx < size; idx += blockDim.x * gridDim.x) {
+            for (size_t idx = threadIdx.x;  idx < shape_dim; idx += blockDim.x) {
                 // one thread per element
-
+                d_out[row_base+idx] = exp(d_data[row_base+idx]-row_max) / row_sum;
             }
         }
 
